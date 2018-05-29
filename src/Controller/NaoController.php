ca45531457d01a7cb84f444b139dabad12c28faf
Stefano <stefano0012@gmail.com>
2018-05-29 21:00:54 +0200

    ajout de la vue twig show

---
+++ src/Controller/NaoController.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stefano
+ * Date: 29/05/18
+ * Time: 20:04
+ */
+
+namespace App\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class NaoController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response("Homepage!");
+    }
+
+    /**
+     * @Route("events")
+     */
+    public function events()
+    {
+        return new Response("events!");
+    }
+
+    /**
+     * @Route("news/{slug}")
+     */
+    public function show($slug)
+    {
+        $comments = [
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla interdum tortor nec finibus ultricies. Vestibulum egestas est nulla. Sed sed lacinia purus, ut commodo nisi.',
+            'Nam nulla mi, vestibulum id iaculis vitae, tincidunt ac turpis. Etiam tincidunt turpis ut erat lobortis, quis elementum massa egestas.',
+            'Phasellus quis bibendum turpis. Pellentesque quis dolor molestie lacus mattis rutrum. Aenean suscipit convallis massa, ut pretium libero volutpat in.'
+        ];
+
+        return $this->render('article/show.html.twig', [
+            'title'     =>  ucwords(str_replace('-', ' ', $slug)),
+            'comments'  => $comments
+        ]);
+    }
+
+    /**
+     * @Route("observation")
+     */
+    public function observation()
+    {
+        return new Response("observation!");
+    }
+
+}
